49f3881b4b2b5b57b268dd2674ddafd372c23201
cesar-cr <cesar.cor.riv@gmail.com>
2021-03-05 11:51:10 -0600

    Ajustes para deployment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 // habilitar express.json
 app.use(express.json({ extended: true }));
 // puerto de APP
-const PORT = process.env.PORT || 4001;
+const port = process.env.PORT || 4001;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -19,6 +19,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tarea', require('./routes/tarea'));
 
-app.listen(PORT, () => {
-    console.log(`El servidor esta escuchando en el servidor:  ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta escuchando en el servidor:  ${port}`);
 })
